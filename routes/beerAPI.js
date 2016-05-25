38a02d0332bb9cf6380ccc263622a77d7cdd630b
bacopa <alicia.fulk@live.com>
2016-05-25 13:04:19 -0700

    got beerObject.data in res.send

--- routes/beerAPI.js
+++ routes/beerAPI.js
@@ -4,25 +4,39 @@ var router = express.Router();
 var request = require('request');
 var beerKey = process.env.beerAPI_KEY;
 
+var beerObject;
 
 router.get("/getbeer", function (req, res) {
-	console.log("/getbeer route");
+	
 	var beerUrl = "http://api.brewerydb.com/v2/beer/random/?key=" + beerKey;
 
-	request(beerUrl, 
+	var beerRequest = request(beerUrl, 
 		function (err, response, body) {
-			console.log("err", err);
-			console.log("response", response);
-			console.log("body", body);
+			if(err){
+				console.log("err", err);
+			}
+
+			//console.log("**** body ****", body);
+			
+			beerObject = JSON.parse(body);
+			if(beerObject){
+				//console.log("beerObject inside request", beerObject);
+				console.log("beerObject inside request beerObject.data", beerObject.data);
+				//res.status(err ? 400 : 200).send(err || JSON.stringify(JSON.parse(body), null, 2));
+				res.send(beerObject.data);
+			}
 	});
 
-	console.log("beerUrl", beerUrl);
+
+
 
 });
+
 
 module.exports = router;
 
 
 // get a random beer!!!!
 // http://api.brewerydb.com/v2/beer/random/?key=31fc9f293dc04a19d57ca04dd0656dbc
+// res.status(err ? 400 : 200).send(err || JSON.stringify(JSON.parse(body), null, 2));
 
